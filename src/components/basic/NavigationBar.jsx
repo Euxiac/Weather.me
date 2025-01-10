46cc0dad1bbeb15aaebadc445b5f6d1d9edd23fc
Christie <christie.tanc@gmail.com>
2025-01-10 15:58:50 +0800

    getting prettier and onchange working together

--- src/components/basic/NavigationBar.jsx
+++ src/components/basic/NavigationBar.jsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-
 import { AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material';
-
 import LogoIcon from '../icons/LogoIcon';
 
 export default function NavigationBar() {
